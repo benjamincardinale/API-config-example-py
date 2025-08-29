0108c4016341820279aee36c7d82f810582747a5
BenjaminCardinale <benjamin.cardinale@ahead.com>
2025-08-29 09:52:25 -0400

    trying to reslove blank output of build

--- export_openapi_yaml.py
+++ export_openapi_yaml.py
@@ -3,5 +3,5 @@ from main import app
 
 if __name__ == "__main__":
     spec = app.openapi()
-    with open("./specs/hellow.openapi.yaml", "w") as f:
+    with open("./specs/openapi.yaml", "w") as f:
         yaml.dump(spec, f, sort_keys=False)
